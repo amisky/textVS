5aa14910901b7807464df7d5ac37288a4328542a
amisky <275077336@qq.com>
2016-05-13 20:26:35 +0800

    Update Default.aspx.cs

--- web/Default.aspx.cs
+++ web/Default.aspx.cs
@@ -10,6 +10,6 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        string sd = "123222222222222222222222221";
+        string sd = "0000";
     }
 }
